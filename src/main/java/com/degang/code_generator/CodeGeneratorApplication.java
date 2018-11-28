54b13bf335a8c5be2272d3475740fe7a23bfdc10
degang <degang@meili-inc.com>
2018-11-28 12:10:54 +0800

    chore:init

---
+++ src/main/java/com/degang/code_generator/CodeGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.degang.code_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeGeneratorApplication.class, args);
+    }
+}
